607892946be562ad625e21a7a5afd8a9fc8aca91
Abd el Hedi Rziga <hedirziga@gmail.com>
2024-03-18 04:10:44 +0100

    updated script

--- script.js
+++ script.js
@@ -1,99 +1,98 @@
 let runningTotal = 0;
-let buffer= "0";
+let buffer = "0";
 let previousOperator;
 const screen = document.querySelector('.screen');
-function buttonClick(value){
-    if(isNaN(value)){
-             handleSymbol(value);
-            }else{
-                handleNumber(value);
-            }
-            screen.innerText= buffer;
-        }
-        
-function handleSymbol(symbol){
-            switch(symbol){
-                case 'C':
-                    buffer= "0";
-                    runningTotal=0;
-                    break;
-                case '=':
-                    if(previousOperator===null){
-                        return;
-                    }
-                    flushOperation(parseInt(buffer));
-                    previousOperator=null;
-                    buffer=runningTotal;
-                    runningTotal=0;
-                    break;
-                case '←':
-                    if(buffer.length===1){
-                        buffer= '0';
-                    }
-                    else{
-                        buffer=buffer.substring(0, buffer.length-1);
-                    } 
-                    break;
-                case '+':
-                case '-':
-                case '×':
-                case '÷':
-                    handleMath(symbol);
-                    break;
-            }
-        }
-        
-function handleMath(symbol){
-            if(buffer=== '0'){
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case '=':
+            if (previousOperator === null) {
                 return;
             }
-            const intBuffer= parseInt(buffer);
-            if(runningTotal === 0){
-                runningTotal=intBuffer;
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
             }
-            else{
-                flushOperation(intBuffer);
+            break;
+        case '+':
+        case '−':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    switch (previousOperator) {
+        case '+':
+            runningTotal += intBuffer;
+            break;
+        case '−':
+            runningTotal -= intBuffer;
+            break;
+        case '×':
+            runningTotal *= intBuffer;
+            break;
+        case '÷':
+            if (intBuffer === 0) {
+                alert("Error: Division by zero");
+                runningTotal = 0;
+            } else {
+                runningTotal /= intBuffer;
             }
-            previousOperator=symbol;
-            buffer='0';
-        }
-        
-function flushOperation(intBuffer){
-            switch(previousOperator){
-                case '+':
-                    runningTotal+=intBuffer;
-                    break;
-                case '-':
-                    runningTotal-=intBuffer;
-                    break;
-                case '×':
-                    runningTotal*=intBuffer;
-                    break;
-                case '÷':
-                    if (intBuffer === 0) {
-                        alert("Error: Division by zero");
-                        runningTotal = 0;
-                    } else {
-                        runningTotal/=intBuffer;
-                    }
-                    break;
-            }
-        }
-        
-function handleNumber(numberString){
-            if(buffer === "0"){
-                buffer=numberString;
-            }else{
-                buffer+=numberString;
-            }
-        }
-        
-function init(){
-            document.querySelectorAll('.calc-button').forEach(button => {
-                button.addEventListener('click', function(event){
-                    buttonClick(event.target.innerText);
-                });
-            });
-        }
-        
+            break;
+    }
+}
+
+function handleNumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
+
+function init() {
+    document.querySelectorAll('.calc-button').forEach(button => {
+        button.addEventListener('click', function (event) {
+            buttonClick(event.target.innerText);
+        });
+    });
+}
+
 init();
